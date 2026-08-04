a265cad72e7e1a491e95cbba82caaaee99afb613
Burzhuys-dice <zavinikita@gmail.com>
2026-08-04 16:29:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ async function fetchAllData() {
         const [advRes, predRes, coreRes, clansRes, discRes, archRes] = await Promise.all([
             fetch('data/vtm_merits_data.json'),
             fetch('data/vtm_predator-types_1'),
-            fetch('data/vtm_char_and_skills'),
             fetch('data.js'),
             fetch('data/vtm_disciplines'),
             fetch('data/vtm_archetypes.json') 
